58af92e266b8685aa570e0a1dc1fd732ac617ab9
Rukayat Sadiq <rukayat.sadiq@andela.co>
2015-07-13 18:00:58 +0100

    Re-style welcome page

--- app/models/patient.rb
+++ app/models/patient.rb
@@ -2,7 +2,8 @@ class Patient < ActiveRecord::Base
 
   has_many :appointments
   has_many :schedulers, through: :appointments
-  validates_presence_of :name
+  validates_presence_of :name, :sex, :email
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
   validates_uniqueness_of :name, :message => "This name has been used"
 
   def self.search(search)
@@ -12,4 +13,10 @@ class Patient < ActiveRecord::Base
       find(:all)
     end
   end
+
+  # t.string   "name"
+  # t.string   "sex"
+  # t.string   "email"
+  # t.datetime "created_at", null: false
+  # t.datetime "updated_at", null: false
 end
